6f9098ccbf09bca4469e654f01b2e1c2611f1da6
Arthur Vieira Lopes <arthur.lopes@feluma.org.br>
2026-04-29 13:09:53 -0300

    add gitignore frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main style={{ flex: 1 }}>
+        <h1>Conteúdo da página</h1>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">ícone</div>
+
+        <nav className="nav">
+          <a href="#">HOTEL</a>
+          <a href="#">ACOMODAÇÕES</a>
+          <a href="#">PACOTES</a>
+          <a href="#">GASTRONOMIA</a>
+          <a href="#">EVENTOS</a>
+          <a href="#">CORPORATIVO</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
