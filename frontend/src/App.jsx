11deb3c476a97524223d8008c1eb305914752989
arthurvieiralopes <vieiraarthur100@gmail.com>
2026-04-30 17:10:27 -0300

    Criação das rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import CadastroUsuario from "../CadastroUsuario";
 
 function App() {
   return (
@@ -7,7 +9,10 @@ function App() {
       <Header />
 
       <main style={{ flex: 1 }}>
-        <h1>Conteúdo da página</h1>
+        <Routes>
+          <Route path="/" element={<h1>Conteúdo da página</h1>} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+        </Routes>
       </main>
 
       <Footer />
