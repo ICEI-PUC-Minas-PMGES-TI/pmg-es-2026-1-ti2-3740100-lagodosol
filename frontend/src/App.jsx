36986fe38a1220155bee09bd4905fbd8fbadc3c3
Nevez6 <arturfneves17@gmail.com>
2026-05-11 18:21:14 -0300

    conexcao do backend com o front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
 import GestaoClientes from "./GestaoClientes";
-<<<<<<< HEAD
 import PagamentoQuarto from "./PagamentoQuarto";
-=======
-import logo from "./assets/logo.jpg";
 import CadastroQuarto from "./CadastroQuarto";
->>>>>>> 2e52e01 (colocando o cadastro de quarto dentro do app principal)
 
 import CadastroUsuario from "./pages/Cadastro";
 import Login from "./pages/Login";
@@ -27,12 +23,10 @@ function App() {
           <Route path="/login" element={<Login />} />
 
           <Route path="/clientes" element={<GestaoClientes />} />
-<<<<<<< HEAD
 
           <Route path="/pagamento" element={<PagamentoQuarto />} />
-=======
+
           <Route path="/quarto" element={<CadastroQuarto />} />
->>>>>>> 2e52e01 (colocando o cadastro de quarto dentro do app principal)
         </Routes>
       </main>
 
